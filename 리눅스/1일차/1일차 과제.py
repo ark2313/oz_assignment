19f8fa20949be47b9633f0665fee263cc80665e9
sangmin <sangmin5719@never.com>
2025-04-11 16:06:08 +0900

    1일차 과제.py

--- 리눅스/1일차/1일차 과제.py
+++ 리눅스/1일차/1일차 과제.py
@@ -5,15 +5,23 @@
    "metadata": {},
    "source": [
     "문제1. oz라는 폴더가 있다는 가정하에 oz 폴더의 위치를 찾는 명령어를 작성해주세요"
-   ]  },
 
-which oz
+Which oz
 
+
+
+   ]
+  },
   {
    "cell_type": "markdown",
    "metadata": {},
    "source": [
     "문제2. 문제 2: 2024년 5월의 달력을 화면에 보여주는 리눅스 명령어를 작성해주세요"
+
+
+Cal 5 2024
+
+
    ]
   },
   {
@@ -21,6 +29,14 @@ which oz
    "metadata": {},
    "source": [
     "문제 3: echo 명령어를 이용해 oz.txt라는 파일을 생성하고 파일안에 “Hello Linux”라는 내용이 들어갈 수 있도록 리눅스 명령어를 작성해주세요"
+
+
+
+ echo "Hello Linux" | oz.txt
+
+
+
+
    ]
   },
   {
@@ -32,6 +48,17 @@ which oz
     "```bash\n",
     "echo \"apple banana orange\" | xargs -n 1 echo\n",
     "```"
+
+
+Apple
+
+Banana
+
+Orange
+
+
+
+
    ]
   },
   {
@@ -46,6 +73,10 @@ which oz
     "4. Cal\n",
     "5. Clear\n",
     "6. which oz\n"
+
+!4
+
+
    ]
   },
   {
@@ -59,6 +90,11 @@ which oz
     "3. Linux 명령은 CommandName -options input 구조를 따릅니다.\n",
     "4. 일부 명령에는 짧은 형식 옵션만 사용할 수 있습니다.\n",
     "5. 긴 형식 옵션에는 대시 1개 (-)가 있고 짧은 형식 옵션에는 대시 2개(--)가 있습니다."
+
+1번
+
+
+
    ]
   },
   {
@@ -71,6 +107,12 @@ which oz
     "2. 네트워크 연결 여부를 확인하는 명령어다\n",
     "3. 입력된 데이터를 모아 한번에 저장하기 위해 사용되는 명령어다\n",
     "4. 삭제된 파일 목록을 확인하기 위한 명령어다"
+
+
+1번
+
+
+
    ]
   },
   {
@@ -78,6 +120,12 @@ which oz
    "metadata": {},
    "source": [
     "문제8. \"example.txt\" 파일에 \"Hello, world!\"라는 문자열을 기록하고, 동시에 터미널에도 출력하려고 합니다. 올바른 tee 명령어를 작성하세요."
+
+
+
+echo "Hello, world!" | tee example.txt
+
+
    ]
   },
   {
@@ -91,6 +139,10 @@ which oz
     "3. Reference\n",
     "4. TEE\n",
     "5. xargs"
+
+1번
+
+
    ]
   },
   {
@@ -98,6 +150,14 @@ which oz
    "metadata": {},
    "source": [
     "문제10. 현재 디렉토리에 여러개의 `.txt` 파일이 있다고 가정해봅시다. 다른 파일은 남기고 `.txt`로 끝나는 파일들만 삭제하기 위해 작성해야 하는 명령어를 작성해보세요"
+
+
+
+
+
+
+
+
    ]
   }
  ],
